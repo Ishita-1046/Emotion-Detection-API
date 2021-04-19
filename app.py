d355c5491f763393d39656b96a2bf14c708d0c75
Ishita-1046 <ishitapatel33333@gmail.com>
2021-04-20 03:06:07 +0530

    Used waitress

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from keras.models import model_from_json,load_model
 from flask import Flask,request, jsonify
 import cv2
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,7 @@ def predict():
 
 if '__name__'=='__main__':
 
-    app.run()
+    #app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
